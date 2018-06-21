d88df5e870be760c9c1538fb26ff041afd861cd6
Tomas Jonasson <tomas@novomaticls.com>
2018-06-21 23:20:07 +0000

    Update stats for 21.06

--- src/index.js
+++ src/index.js
@@ -8,100 +8,100 @@ import Chart from "chart.js";
 function getPlayerData(player) {
   // Albert
   if (player === "albert") {
-    return [20253, 22454, 24692, 26286];
+    return [20253, 22454, 24692, 26286, 26826];
   }
   // Alfreð
   else if (player === "alfred") {
-    return [76753, 81485, 85919, 88550];
+    return [76753, 81485, 85919, 88550, 90022];
   }
   // Ari
   else if (player === "ari") {
-    return [21628, 23612, 25529, 26645];
+    return [21628, 23612, 25529, 26645, 27372];
   }
   // Arnór
   else if (player === "arnor") {
-    return [16976, 17355, 17586, 18156];
+    return [16976, 17355, 17586, 18156, 18291];
   }
   // Aron
   else if (player === "aron") {
-    return [96394, 101688, 107104, 110352];
+    return [96394, 101688, 107104, 110352, 112367];
   }
   // Birkir Bjarna
   else if (player === "birkir_bjarna") {
-    return [96515, 106054, 114103, 119749];
+    return [96515, 106054, 114103, 119749, 123467];
   }
   // Birkir Már
   else if (player === "birkir_mar") {
-    return [7559, 7922, 8058, 8148];
+    return [7559, 7922, 8058, 8148, 8316];
   }
   // Björn
   else if (player === "bjorn") {
-    return [5354, 5354, 5354, 5611];
+    return [5354, 5354, 5354, 5611, 6248];
   }
   // Emil
   else if (player === "emil") {
-    return [14939, 15787, 17041, 17902];
+    return [14939, 15787, 17041, 17902, 18451];
   }
   // Frederik
   else if (player === "frederik") {
-    return [6696, 7862, 9125, 11202];
+    return [6696, 7862, 9125, 11202, 12053];
   }
   // Gylfi
   else if (player === "gylfi") {
-    return [180146, 184401, 187716, 189752];
+    return [180146, 184401, 187716, 189752, 191664];
   }
   // Hannes
   else if (player === "hannes") {
-    return [44568, 47966, 50839, 52940];
+    return [44568, 47966, 50839, 52940, 54536];
   }
   // Hólmar
   else if (player === "holmar") {
-    return [15207, 16176, 16731, 17557];
+    return [15207, 16176, 16731, 17557, 18175];
   }
   // Hörður
   else if (player === "hordur") {
-    return [31407, 33751, 36159, 40189];
+    return [31407, 33751, 36159, 40189, 42068];
   }
   // Jóhann Berg
   else if (player === "johann") {
-    return [36951, 39928, 42640, 44390];
+    return [36951, 39928, 42640, 44390, 45515];
   }
   // Jón Daði
   else if (player === "jon") {
-    return [23314, 24732, 26013, 26848];
+    return [23314, 24732, 26013, 26848, 27552];
   }
   // Kári
   else if (player === "kari") {
-    return [567, 567, 1495, 1768];
+    return [567, 567, 1495, 1768, 1890];
   }
   // Ragnar
   else if (player === "ragnar") {
-    return [37182, 41012, 44148, 46087];
+    return [37182, 41012, 44148, 46087, 47364];
   }
   // Rúnar Alex
   else if (player === "runar") {
-    return [6874, 7570, 8145, 8552];
+    return [6874, 7570, 8145, 8552, 8839];
   }
   // Rúrik
   else if (player === "rurik") {
-    return [319994, 428081, 577867, 678632];
+    return [319994, 428081, 577867, 678632, 733783];
   }
   // Samúel
   else if (player === "samuel") {
-    return [9970, 10366, 10636, 11262];
+    return [9970, 10366, 10636, 11262, 11460];
   }
   // Sverrir Ingi
   else if (player === "sverrir") {
-    return [8257, 8833, 9380, 9767];
+    return [8257, 8833, 9380, 9767, 9998];
   }
   // Ólafur Skúlason
   else if (player === "olafur") {
-    return [1501, 2588, 2874, 2998];
+    return [1501, 2588, 2874, 2998, 3113];
   }
 }
 
 function getDates() {
-  return ["17.06", "18.06", "19.06", "20.06"];
+  return ["17.06", "18.06", "19.06", "20.06", "21.06"];
 }
 
 var myChart;
